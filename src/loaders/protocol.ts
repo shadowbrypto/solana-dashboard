1c23856da9f2b26a22b8d56fdd81638c6f2c8ca4
Paaji <89406655+crypto-paaji@users.noreply.github.com>
2025-04-15 23:45:19 +0530

    make env great again

--- src/loaders/protocol.ts
+++ src/loaders/protocol.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 export async function queryProtocolData(protocol = "bullx") {
